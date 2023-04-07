1357faf7a3ccb06a78c5b2d12c6296e31796f7be
xiangyi1106 <xiangyicheng03@gmail.com>
2023-04-07 13:40:15 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:adventuregame/story_brain.dart';
 import 'package:flutter/material.dart';
-
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
 void main() => runApp(AdventureGame());
 
@@ -17,6 +16,7 @@ class AdventureGame extends StatelessWidget {
 }
 
 //TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+StoryBrain storyBrain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
   @override
@@ -31,6 +31,12 @@ class _StoryPageState extends State<StoryPage> {
         //TODO: Step 1 - Find a background image, add the background image into the images directory, then add the background image to this Container.
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -40,8 +46,10 @@ class _StoryPageState extends State<StoryPage> {
                 child: Center(
                   child: Text(
                     //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here',
+                    storyBrain.getStory(),
+                    textAlign: TextAlign.justify,
                     style: TextStyle(
+                      fontFamily: 'PTSerif',
                       fontSize: 25.0,
                     ),
                   ),
@@ -53,6 +61,9 @@ class _StoryPageState extends State<StoryPage> {
                   onPressed: () {
                     // Choice 1 made by user
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
                   },
                   style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.resolveWith(
@@ -64,8 +75,9 @@ class _StoryPageState extends State<StoryPage> {
                   ),
                   child: Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoice1(),
                     style: TextStyle(
+                      fontFamily: 'PTSerif',
                       fontSize: 20.0,
                     ),
                   ),
@@ -79,10 +91,14 @@ class _StoryPageState extends State<StoryPage> {
                 child: Visibility(
                   //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this TextButton.
                   //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                  visible: storyBrain.buttonShouldBeVisible(),
                   child: TextButton(
                     onPressed: () {
                       // Choice 2 made by user
                       //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
                     },
                     style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.resolveWith(
@@ -94,8 +110,9 @@ class _StoryPageState extends State<StoryPage> {
                     ),
                     child: Text(
                       //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                      'Choice 2',
+                      storyBrain.getChoice2(),
                       style: TextStyle(
+                        fontFamily: 'PTSerif',
                         fontSize: 20.0,
                       ),
                     ),
